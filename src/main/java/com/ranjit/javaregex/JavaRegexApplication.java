fecb50df2ad8bc119e8964c5bf2b545659feb804
ranjit <ranjit.patel@billpay.de>
2020-08-01 14:52:42 +0200

    initial commit

---
+++ src/main/java/com/ranjit/javaregex/JavaRegexApplication.java
@@ -0,0 +1,13 @@
+package com.ranjit.javaregex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRegexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRegexApplication.class, args);
+	}
+
+}
